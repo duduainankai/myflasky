d2c4e066c7d7205737f7208905e2715d987f0979
lizheming <nkdudu@126.com>
2016-07-05 23:51:40 +0800

    用户注册控制器及界面

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,9 +4,10 @@
 
 from . import auth
 from flask import render_template, url_for, redirect, flash, request
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 from ..models import User
 from flask.ext.login import login_user, logout_user, login_required
+from .. import db
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -17,7 +18,7 @@ def login():
 		if user is not None and user.verify_password(form.password.data):
 			login_user(user, form.remember_me.data)
 			# 用户访问未授权的URL时会显示登录表单， Flask－Login会把原地址保存在查询字符串的next参数
-			return redirect(request.args.get('next') or url_for('mail.index'))
+			return redirect(request.args.get('next') or url_for('main.index'))
 		flash('Invalid username or password')
 	return render_template('auth/login.html', form=form)
 
@@ -28,3 +29,16 @@ def logout():
 	logout_user()
 	flash('You have been logged out.')
 	return redirect(url_for('auth.login'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+	form = RegisterForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data, 
+			password=form.password.data)
+		db.session.add(user)
+		flash('You can now login')
+		return redirect(url_for('auth.login'))
+	return render_template('auth/register.html', form=form)
+
