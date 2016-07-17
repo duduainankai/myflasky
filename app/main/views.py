18c0027b5858a95bda4b826bd81eb753bc63916d
lizheming <nkdudu@126.com>
2016-07-17 19:41:27 +0800

    用户资料编辑器

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,11 @@
 # !/usr/bin/env python
 #  encoding: utf-8
 
-from flask import render_template, abort
+from flask import render_template, abort, redirect, url_for, flash
 from ..models import User
+from .forms import EditProfileForm
+from .. import db
+from flask.ext.login import current_user, login_required
 
 from . import main
 
@@ -18,3 +21,20 @@ def user(username):
 	if not user:
 		abort(404)
 	return render_template("user.html", user=user)
+
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+	form = EditProfileForm()
+	if form.validate_on_submit():
+		current_user.name = form.name.data
+		current_user.location = form.location.data
+		current_user.about_me = form.about_me.data
+		db.session.add(current_user)
+		flash('Your profile has been updated.')
+		return redirect(url_for('.user', username=current_user.username))
+	form.name.data = current_user.name
+	form.location.data = current_user.location
+	form.about_me.data = current_user.about_me
+	return render_template("edit_profile.html", form=form)
