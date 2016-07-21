4bdc2a8d3e6b69428cfcf8669d07a57d32c43289
lizheming <nkdudu@126.com>
2016-07-22 00:46:56 +0800

    解决markdown bug

--- app/models.py
+++ app/models.py
@@ -208,9 +208,9 @@ class Post(db.Model):
 	__tablename__ = 'posts'
 	id = db.Column(db.Integer, primary_key=True)
 	body = db.Column(db.Text)
+	body_html = db.Column(db.Text)
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-	body_html = db.Column(db.Text)
 
 	@staticmethod
 	def generate_fake(count=100):
@@ -222,7 +222,7 @@ class Post(db.Model):
 		for i in range(count):
 			u = User.query.offset(randint(0, user_count - 1)).first()
 			p = Post(body=forgery_py.lorem_ipsum.sentences(randint(1,3)),
-				timestamp=forgery_py.date.date(True),
+					timestamp=forgery_py.date.date(True),
 				author=u)
 			db.session.add(p)
 			db.session.commit()
@@ -232,7 +232,8 @@ class Post(db.Model):
 		allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
                         'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
                         'h1', 'h2', 'h3', 'p']
-        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'),
-        	tags=allowed_tags, strip=True))
+		a = markdown(value, output_format='html')
+		b = bleach.clean(a, tags=allowed_tags, strip=True)
+		target.body_html = bleach.linkify(b)
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
