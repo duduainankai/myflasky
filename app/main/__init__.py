4c4b3ffb754165e694e883b07215416d6cb69dc7
lizheming <nkdudu@126.com>
2016-08-09 23:40:17 +0800

    管理评论

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -21,4 +21,4 @@ from ..models import Permission
 #上下文处理器能让变量在所有模板中全局可访问
 @main.app_context_processor
 def inject_permissions():
-	return dict(Permisson=Permission)
+	return dict(Permission=Permission)
