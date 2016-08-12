ed7f5e072318437cfe076eaa632692f547689c73
lizheming <nkdudu@126.com>
2016-08-12 23:22:47 +0800

    资源和json的序列化转换

--- app/models.py
+++ app/models.py
@@ -6,10 +6,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask.ext.login import UserMixin, AnonymousUserMixin	# 包含了四个方法的默认实现
 from . import login_manager
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app, request
+from flask import current_app, request, url_for
 import hashlib
 from markdown import markdown
 import bleach
+from exceptions import ValidationError
 
 from datetime import datetime
 
@@ -214,6 +215,18 @@ class User(UserMixin, db.Model):
 			return None
 		return User.query.get(data['id'])
 
+	def to_json(self):
+		json_user = {
+			'url': url_for('api.get_user', id=self.id, _external=True)
+			'username': self.username,
+			'member_since': self.member_since,
+			'last_seen': self.last_seen,
+			'posts': url_for('api.get_user_posts', id=self.id, _external=True),
+			'followed_posts': url_for('api.get_user_followed_posts', id=self.id, _external=True),
+			'post_count': self.posts.count()
+		}
+		return json_user
+
 
 # flask－login要求实现的回调函数 使用指定的标识符加载用户
 @login_manager.user_loader
@@ -302,6 +315,25 @@ class Post(db.Model):
 		a = markdown(value, output_format='html')
 		b = bleach.clean(a, tags=allowed_tags, strip=True)
 		target.body_html = bleach.linkify(b)
+
+	def to_json(self):
+		json_post = {
+			'url': url_for('api.get_post', id=self.id, _external=True),
+			'body': self.body,
+			'body_html': self.body_html,
+			'timestamp': self.timestamp,
+			'author': url_for('api.get_user', id=self.author_id, _external=True),
+			'comments': url_for('api.get_comments', id=self.id, _external=True),
+			'comment_count': self.comments.count()
+		}
+		return json_post
+
+	@staticmethod
+	def from_json(json_post):
+		body = json_post.get('body')
+		if body is None or body == '':
+			raise ValidationError('post does not have a body')
+		return Post(body=body)
 
 db.event.listen(Post.body, 'set', Post.on_changed_body)
 
